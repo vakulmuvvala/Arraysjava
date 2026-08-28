df91bc3a0005ff13866128ea23c00843bb1fff5d
Vakul Muvvala <vakulmuvvala@gmail.com>
2026-08-28 16:55:10 -0400

    My first array code

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,72 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.util.Scanner;
+
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//void main() {
+//    System.out.println("Hello world");
+//}
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //int num= sc.nextInt();
+        //num=num+1;
+
+        //decleration
+        int[] student = new int[5];
+        int[] student2 = new int[5];
+
+        //initializiation
+        student = new int[]{99, 85, 76, 78, 90};
+        student2 = new int[]{95, 94, 91, 90, 89};
+
+        int[] student3 = {99, 98, 97, 96, 95};
+        //99->student3[0]
+        //98->student3[1]
+        //95->student3[4]
+
+        System.out.println(student3[3]);
+
+        System.out.println(student[2]);
+
+        //create 3 variables  avg1, avg2,avg3
+        //using for loop store the average of 3 students in that variable
+
+
+        int avg1 = 0;
+        int avg2 = 0;
+        int avg3 = 0;
+        for (int i = 0; i < 5; i++) {
+            avg1 = avg1+  student[i];
+            avg2 = avg2 + student2[i];
+            avg3 = avg3 + student3[i];
+
+        }
+        avg1 = avg1 / 5;
+        System.out.println("avg1 -" + avg1);
+        avg2 = avg2 / 5;
+        System.out.println("avg2 -" + avg2);
+        avg3 = avg3 / 5;
+        System.out.println(+avg3);
+
+        // arr[]={s1,s2,s3 .... s100}
+        //stu[]={marks1,marks2...}
+        // for(students){
+        //for(marks){
+    //}
+
+
+
+
+
+
     }
+
+
+    static void methodVakul(){
+        System.out.println("Hello vakul");
+    }
+
 }
